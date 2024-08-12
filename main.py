9e485cd238747429296f5af03b68785ce051cb57
root <sontapaa.jokulainen@gmail.com>
2024-08-12 19:54:48 +0300

    Oof

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 from generic_mutator import *
 
 if __name__=="__main__":
-	input_data = "somedata"
-	COUNT = 100
+	input_data = ""
+	COUNT = 100000
 	for _ in range(COUNT):
-		input_data = mutate_generic(input_data)
-		print(input_data)
+		mutate_generic(input_data)
+		#print(input_data)
 	exit(0)
